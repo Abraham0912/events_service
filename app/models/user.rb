4d3c2c7166504850f831dbce2ec67d8ba2c0dcdc
Abraham0912 <09120912ahn@gmail.com>
2021-11-10 16:38:50 -0600

    install pundit and config initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #Establecer valor de rol por defecto
   after_initialize do
     if self.new_record?
       self.role ||= :standard
     end
   end
+  
   devise :database_authenticatable, :registerable,
          #:recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
