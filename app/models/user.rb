429c4ee0bdde0ca7b4d6fe155b61d2a192698192
Abraham0912 <09120912ahn@gmail.com>
2021-11-10 19:44:44 -0600

    enter

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
       self.role ||= :standard
     end
   end
-  
+
   devise :database_authenticatable, :registerable,
          #:recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
