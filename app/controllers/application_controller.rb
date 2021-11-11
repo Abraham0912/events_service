88c8b5e1c5b259a01e8f675cb86b1ae54187d1dd
Abraham0912 <09120912ahn@gmail.com>
2021-11-11 12:50:03 -0600

    add value to enum and return message nopermisos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
     include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    private
+    def user_not_authorized
+        render json: {message:'No tiene suficientes permisos'}
+    end
 end
